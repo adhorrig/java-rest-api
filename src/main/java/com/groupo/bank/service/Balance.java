5b71d9778a87ff76085647fc37e377a0def81ac3
Anthony Bloomer <ant0@protonmail.ch>
2016-12-14 17:46:47 +0000

    Get balance

---
+++ src/main/java/com/groupo/bank/service/Balance.java
@@ -0,0 +1,17 @@
+package com.groupo.bank.service;
+
+/**
+ *
+ * @author anthony
+ */
+public class Balance {
+    
+    String account;
+    String balance;
+
+    public Balance(String account, String balance) {
+        this.account = account;
+        this.balance = balance;
+    }
+    
+}
