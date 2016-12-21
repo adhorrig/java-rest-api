118c069ef29bb09460b55a47392e3e6aede6bc00
Anthony Bloomer <ant0@protonmail.ch>
2016-12-21 02:36:09 +0000

    Should do it

--- client/js/main.js
+++ client/js/main.js
@@ -5,43 +5,27 @@
 var api = 'http://0.0.0.0:8080';
 var key = '3cf0e880-a782-4ce6-a63c-7ae95891051f';
 
-<<<<<<< HEAD
 
-function get_data(method, url) {
+function get_data(method, url){
     console.log(url);
     console.log(method);
-=======
-function get_data(method, url){
->>>>>>> 37b2c492410802395468c7ab8371186692489fd9
     $.ajax({
         url: url,
         dataType: 'text',
         type: method,
-        async: false,
         success: function (data) {
-            console.log('success');
+            console.log(data);
             $('.message-block').show("slide");
             $('.message').text(data);
         },
         error: function (xhr, status, error) {
             $('.message-block').show("slide");
-<<<<<<< HEAD
-            $('.message').text(error);
-=======
+            console.log(xhr);
+            console.log(status);
             console.log(error);
             $('.data').text(xhr);
->>>>>>> 37b2c492410802395468c7ab8371186692489fd9
         }
     });
-}
-
-function get_customers() {
-    var params = {
-        api_key: key
-    };
-
-    get_data('GET', api + '/api/customer?' + jQuery.param(params));
-
 
 }
 
@@ -50,22 +34,26 @@ $("#transfer").click(function (e) {
     var from = $('#from').val();
     var to = $('#to').val();
     var amount = $('#amount').val();
+
     var params = {
         from: from,
         to: to,
         amount: amount,
         api_key: key
     };
+
     get_data('POST', api + '/api/transfer/create?' + jQuery.param(params));
 });
 
 
 $("#get-balance").click(function (e) {
     var account = $('#account').val();
+
     var params = {
         account_number: account,
         api_key: key
     };
+
     get_data('GET', api + '/api/balance?' + jQuery.param(params));
 });
 
@@ -73,6 +61,7 @@ $("#get-balance").click(function (e) {
 $("#withdrawl").click(function (e) {
     var account = $('#account_number').val();
     var amount = $('#amount').val();
+
     var params = {
         account: account,
         amount: amount,
@@ -80,6 +69,28 @@ $("#withdrawl").click(function (e) {
     };
 
     get_data('POST', api + '/api/withdrawl?' + jQuery.param(params));
+});
+
+
+$("#add-customer").click(function (e) {
+    var customer_name = $('#name').val();
+    var email = $('#email').val();
+    var password = $('#password').val();
+    var address = $('#address-input').val();
+    var option = $("#custom-select").val();
+
+    var params = {
+        name: customer_name,
+        email: email,
+        address: address,
+        password: password,
+        account_type: option,
+        api_key: key
+    };
+
+    get_data('POST', api + '/api/customer/create?' + jQuery.param(params));
+
+
 });
 
 $("#lodgement").click(function (e) {
@@ -94,30 +105,3 @@ $("#lodgement").click(function (e) {
 
     get_data('POST', api + '/api/lodgement?' + jQuery.param(params));
 });
-
-
-$("#add-customer").click(function (e) {
-    var customer_name = $('#name').val();
-    var email = $('#email').val();
-    var password = $('#password').val();
-    var address = $('#address-input').val();
-    var option = $("#custom-select").val();
-    var params = {
-        name: customer_name,
-        email: email,
-        address: address,
-        password: password,
-        account_type: option,
-        api_key: key
-    };
-    get_data('POST', api + '/api/customer/create?' + jQuery.param(params));
-<<<<<<< HEAD
-    get_customers();
-
-
-});
-=======
-    var item = document.getElementById("result");
-    item.innerHTML = 'Account has been created';
-});
->>>>>>> 37b2c492410802395468c7ab8371186692489fd9
